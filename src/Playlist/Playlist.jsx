4804f907cdcc2423d0d564f03c4a517a4aed1fe3
Vanessa <vanessa.ferreira.dev@gmail.com>
2023-03-19 19:45:23 +0000

    Add musics and AlbumCovers

--- src/Playlist/Playlist.jsx
+++ src/Playlist/Playlist.jsx
@@ -1,18 +1,29 @@
 const playlist = [
-    {
-      image: "/assets/happy.jpg",
-      title: "Happy",
-      artist: "Pharrel",
-      src: "/assets/Pharrell Williams - Happy (From Despicable Me 2).mp3",
-    },
-    {
-      image: "/assets/tom odell.jpg",
-      title: "Another Love",
-      artist: "Tom Odell",
-      src: "/assets/Tom Odell - Another Love.mp3",
-    },
-    // Add more songs
-  ];
-  
-  export default playlist;
-  
+  {
+    image: "/assets/fatboyslim.jpg",
+    title: "Right Here, Right Now",
+    artist: "Fatboy Slim",
+    src: "/assets/Right Here Right Now.mp3",
+  },
+  {
+    image: "/assets/offspring.jpg",
+    title: "The Kids Aren't Alright",
+    artist: "Offspring",
+    src: "/assets/The Offspring - The Kids Arent Alright.mp3",
+  },
+  {
+    image: "/assets/happy.jpg",
+    title: "Happy",
+    artist: "Pharrel",
+    src: "/assets/Pharrell Williams - Happy (From Despicable Me 2).mp3",
+  },
+  {
+    image: "/assets/tom odell.jpg",
+    title: "Another Love",
+    artist: "Tom Odell",
+    src: "/assets/Tom Odell - Another Love.mp3",
+  },
+  // Add more songs
+];
+
+export default playlist;
